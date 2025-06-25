5697dc4be6cb17906980a6653a93a53cd61c3ebb
IsabellaT29 <115107520+IsabellaT29@users.noreply.github.com>
2025-06-24 22:13:30 -0400

    Criando os models das tabelas

---
+++ SistemaPlanejaGo/src/main/java/com/mycompany/sistemaplanejago/model/Categoria.java
@@ -0,0 +1,60 @@
+package com.mycompany.sistemaplanejago.model;
+
+
+public class Categoria {
+    private int id; 
+    private String titulo; 
+    private String descricao; 
+    private int tipo; 
+
+    public Categoria() {
+    }
+
+   
+    public Categoria(int id, String titulo, String descricao, int tipo) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.tipo = tipo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+ 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    
+    public String getTitulo() {
+        return titulo;
+    }
+
+ 
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    
+    public String getDescricao() {
+        return descricao;
+    }
+
+   
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    
+    public int getTipo() {
+        return tipo;
+    }
+
+   
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+}
